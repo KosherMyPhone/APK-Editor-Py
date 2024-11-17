3a2e88d06b8a1761ab9c12f5fd408635e3456f15
Shlomo Zalman Rabinowitz <106286969+SZRabinowitz@users.noreply.github.com>
2024-11-16 22:02:53 -0500

    Added docstring for find_activity_or_class method

--- src/apk_editor/smali.py
+++ src/apk_editor/smali.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from apk_editor import APK
 from apk_editor.apk import DecompiledAPK
 
 
@@ -8,6 +7,14 @@ class SmaliUtils:
         self.apk = decompiled_apk
 
     def find_activity_or_class(self, class_name: str) -> Path | None:
+        """Find the smali file for the given class name.
+
+        Args:
+            class_name (str): The class name to search for.
+
+        Returns:
+            Path | None: The path to the smali file if found, None otherwise.
+        """
         base_name = class_name.split(".")[-1]
         for root, dirs, files in self.apk.smali_path.walk():
             for f in files:
