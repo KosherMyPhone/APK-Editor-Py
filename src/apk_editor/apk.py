5db93ae98b471e64719648bd0c0756c729d15897
Shlomo Zalman Rabinowitz <106286969+SZRabinowitz@users.noreply.github.com>
2024-11-17 19:55:26 -0500

    Add method get_apk_info in apk module also, add attribute decompiled_apk in APK class object.

--- src/apk_editor/apk.py
+++ src/apk_editor/apk.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+import json
 from pathlib import Path
 import subprocess
 from apk_editor.constants import apkeditor
@@ -15,6 +17,7 @@ class APK:
     def __init__(self, path: Path):
         self.path = path
         self.temp_dir: TemporaryDirectory = None
+        self.decompiled_apk: DecompiledAPK = None
 
     def decompile(self) -> DecompiledAPK:
         self.temp_dir = TemporaryDirectory()
@@ -36,7 +39,39 @@ class APK:
             capture_output=True,
         )
 
-        return DecompiledAPK(output_dir)
+        self.decompiled_apk = DecompiledAPK(output_dir)
+        return self.decompiled_apk
 
     def cleanup(self):
         self.temp_dir.cleanup()
+
+
+APKInfo = namedtuple(
+    "APKInfo",
+    [
+        "package_name",
+        "version_code",
+        "version_name",
+        "app_name",
+        "app_icon_path",
+        "application_class",
+        "main_activity",
+    ],
+)
+
+
+def get_apk_info(apk_path: Path) -> APKInfo:
+    get_info_cmd = ["java", "-jar", apkeditor, "info", "-i", apk_path, "-t", "json"]
+    result = subprocess.run(get_info_cmd, check=True, capture_output=True)
+    results = json.loads(result.stdout)
+    res_fmt = {list(d.keys())[0]: list(d.values())[0] for d in results}
+    # return res_fmt
+    return APKInfo(
+        res_fmt["package"],
+        res_fmt["VersionCode"],
+        res_fmt["VersionName"],
+        res_fmt["AppName"],
+        res_fmt["AppIcon"],
+        res_fmt["application-class"],
+        res_fmt["activity-main"],
+    )
